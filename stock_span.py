e75a98cb2749c36cc7bc4406fb0d597b7bf2fa47
mukul2604 <mukul312@gmail.com>
2016-09-26 13:48:03 -0400

    NO one killed jessica

--- stock_span.py
+++ stock_span.py
@@ -1,4 +1,29 @@
 #! /usr/bin/python
 import sys
 stocks = map(int,raw_input().strip().split())
-print stocks
+n = len(stocks)
+
+st_stack = [0]
+res = [1]
+
+def top_index(list):
+	return list[len(list)-1]
+
+for i in xrange(1,n):
+	if stocks[i] < stocks[top_index(st_stack)]:
+		span = i - top_index(st_stack)
+		res.append(span)
+		st_stack.append(i)
+	else:
+		while (len(st_stack) > 0 and stocks[i] >= stocks[top_index(st_stack)]):
+			st_stack.pop()
+		
+		if len(st_stack) == 0:
+			span = i+1
+			res.append(span)
+			st_stack.append(i)
+		else:
+			span = i - top_index(st_stack)
+			res.append(span)
+			st_stack.append(i)	
+print res
